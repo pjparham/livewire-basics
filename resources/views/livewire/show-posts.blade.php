e376920ced0f6e542a416b53e82e84fafdcc3082
parkerparham <parkerparham@gmail.com>
2024-04-08 12:55:57 -0400

    finish 'Nesting'

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -10,20 +10,9 @@
         </tr>
         </thead>
         <tbody>
-            @foreach($posts as $post)
-                <tr wire:key="{{ $post->id }}">
-                    <td>{{ $post->title }}</td>
-                    <td>{{ str($post->content)->words(8) }}</td>
-                    <td><button
-                            type="button"
-                            wire:click="delete({{ $post->id }})"
-                            wire:confirm="Are you sure you want to delete this Post?"
-                        >
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-            @endforeach
+        @foreach ($posts as $post)
+            <livewire:post-row :key="$post->id" :$post>
+        @endforeach
         </tbody>
     </table>
 </div>
