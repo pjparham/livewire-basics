4441a8070b4d9e4b143b76162575c3699dbc215d
parkerparham <parkerparham@gmail.com>
2024-04-08 14:43:23 -0400

    finish Form Essentials 'Showing a Success message'

--- app/Livewire/EditProfile.php
+++ app/Livewire/EditProfile.php
@@ -11,6 +11,8 @@ class EditProfile extends Component
     public $username = '';
     public $bio = '';
 
+    public $showSuccessIndicator = false;
+
     public function mount()
     {
         $this->user = User::find(1);
@@ -25,6 +27,8 @@ class EditProfile extends Component
         $this->user->bio = $this->bio;
 
         $this->user->save();
+        sleep(1);
+        $this->showSuccessIndicator = true;
     }
 
     public function render()
