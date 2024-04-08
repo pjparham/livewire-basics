085092e4e9dc2d94c35429a3cd8dd09c4fd0640e
parkerparham <parkerparham@gmail.com>
2024-04-08 11:40:49 -0400

    finish 'Alpine'

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,6 +1,17 @@
 <div>
     <h2>New Post:</h2>
 
+    Current Title: <span x-text="$wire.title"></span>
+<br/>
+    <button x-on:click="$wire.title=''">clear title</button>
+
+    <div x-data="{count: 0}">
+        <span x-text="count"></span>
+
+        <button x-on:click="count++">+</button>
+        <button x-on:click="count--">-</button>
+    </div>
+
     <form wire:submit="save">
         <label>
             <span>Title</span>
@@ -11,6 +22,9 @@
         <label>
             <span>Content</span>
             <textarea wire:model="content"></textarea>
+            <small>Characters:
+                <span x-text="$wire.content.length"></span>
+            </small>
             @error('content') <em> {{$message}}</em>@enderror
         </label>
 
