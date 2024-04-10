4c94b122f6d1f7b2e721aacf0240df6189dd8aea
parkerparham <parkerparham@gmail.com>
2024-04-10 11:01:06 -0400

    finish Modals 'Building a modal'

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,9 +7,13 @@
         <link rel="stylesheet" href="{{ asset('app.css') }}">
 {{--        {{dd(asset('app.css'))}}--}}
         @vite('resources/css/app.css')
+        <script defer src="https://unpkg.com/@alpinejs/ui@3.13.1-beta.0/dist/cdn.min.js"></script>
         <title>{{ $title ?? 'Livewire Basics' }}</title>
     </head>
     <body>
+    <main class="flex justify-center items-center bg-slate-200 min-h-screen text-slate-800">
+        {{ $slot }}
+    </main>
 {{--    <nav>--}}
 {{--        <a wire:navigate href="/" @class(['current' => request()->is('/')]) >Todos</a>--}}
 {{--        <a wire:navigate href="/counter"  @class(['current' => request()->is('counter')])>Counter</a>--}}
@@ -17,6 +21,6 @@
 {{--        <a wire:navigate href="/create-post"  @class(['current' => request()->is('create-post')])>Create a Post</a>--}}
 {{--    </nav>--}}
 
-        {{ $slot }}
+
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::get('/counter', Counter::class);
 Route::get('/show-posts', \App\Livewire\ShowPosts::class);
 Route::get('/create-post', \App\Livewire\CreatePost::class);
 Route::get('/profile', \App\Livewire\EditProfile::class);
+Route::get('/signup', \App\Livewire\Signup::class);
